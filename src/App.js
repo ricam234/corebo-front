3026fe34bf60fc602ac7db52abcb3cc504136fd7
ricam234 <ricardo_22_febrero@hotmail.es>
2026-03-28 12:00:37 -0600

    nuevo modulo de carreras

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login'; 
