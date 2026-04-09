75cf874605c9d737d8b8fc9c4885ccbe66cea6b5
ricam234 <ricardo_22_febrero@hotmail.es>
2026-04-09 06:47:58 -0600

    metodo de pago

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://www.corebocuautla.com.mx/admin/public/";
+const API_URL = "https://www.corebocuautla.com.mx/admin/public";
 
 export const logout = () => {
   localStorage.removeItem("token");
